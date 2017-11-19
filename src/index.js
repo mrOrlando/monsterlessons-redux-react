04de8f4fa30d52d00b7e6a09c91ef22ad168dbb0
mrOrlando <juriy88@mail.com>
2017-11-19 14:59:11 +0300

    Use Redux with HTML.

--- src/index.js
+++ src/index.js
@@ -23,9 +23,26 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
+const list = document.querySelector('.list');
+const trackInput = document.querySelector('.trackInput');
+const addTrackButton = document.querySelector('.addTrack');
+
 store.subscribe(function() {
   console.log('call subscribe, state:', store.getState());
+  list.innerHTML = '';
+  trackInput.value = '';
+  store.getState().forEach(item => {
+    const li = document.createElement('li');
+    li.textContent = item;
+    list.appendChild(li);
+  });
 });
 
 store.dispatch({ type: 'ADD_ITEM', payload: 'Make homework!' });
 store.dispatch({ type: 'ADD_ITEM', payload: 'To smile!' });
+
+addTrackButton.addEventListener('click', () => {
+  const trackName = trackInput.value;
+  console.log('trackName', trackName);
+  store.dispatch({ type: 'ADD_ITEM', payload: trackName });
+});
