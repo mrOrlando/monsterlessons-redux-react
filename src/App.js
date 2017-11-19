d5eb7fcc7de7dbb6a5ca53988e7e8d98898a41bd
mrOrlando <juriy88@mail.com>
2017-11-19 15:52:47 +0300

    Add items through dispatch method to store.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class App extends Component {
+  addTrack = () => {
+    console.log('call addTrack', this.trackInput.value);
+    this.props.onTrackName(this.trackInput.value);
+    this.trackInput.value = '';
+  };
   render() {
     console.log('testStore', this.props.testStore);
     return (
       <div>
-        <input type="text" />
-        <button>Add track</button>
+        <input type="text" ref={input => (this.trackInput = input)} />
+        <button onClick={this.addTrack}>Add track</button>
         <ul>
           {this.props.testStore.map((item, index) => (
             <li key={index}>{item}</li>
@@ -18,4 +23,10 @@ class App extends Component {
   }
 }
 
-export default connect(state => ({ testStore: state }), dispatch => ({}))(App);
+export default connect(
+  state => ({ testStore: state }),
+  dispatch => ({
+    onTrackName: trackName =>
+      dispatch({ type: 'ADD_ITEM', payload: trackName }),
+  }),
+)(App);
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const initialState = ['Make homework!', 'To smile!'];
+const initialState = ['Ed Sheeran - Shape Of You', "Ed Sheeran - Don't"];
 
 function reducer(state = [], action) {
   console.log('call reducer', state, action);
@@ -29,43 +29,3 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 registerServiceWorker();
-
-/* import { createStore } from 'redux';
-
-function reducer(state = [], action) {
-  console.log('call reducer', state, action);
-  if (action.type === 'ADD_ITEM') {
-    return [...state, action.payload];
-  }
-  return state;
-}
-
-const store = createStore(
-  reducer,
-  [],
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
-
-const list = document.querySelector('.list');
-const trackInput = document.querySelector('.trackInput');
-const addTrackButton = document.querySelector('.addTrack');
-
-store.subscribe(function() {
-  console.log('call subscribe, state:', store.getState());
-  list.innerHTML = '';
-  trackInput.value = '';
-  store.getState().forEach(item => {
-    const li = document.createElement('li');
-    li.textContent = item;
-    list.appendChild(li);
-  });
-});
-
-store.dispatch({ type: 'ADD_ITEM', payload: 'Make homework!' });
-store.dispatch({ type: 'ADD_ITEM', payload: 'To smile!' });
-
-addTrackButton.addEventListener('click', () => {
-  const trackName = trackInput.value;
-  console.log('trackName', trackName);
-  store.dispatch({ type: 'ADD_ITEM', payload: trackName });
-}); */
