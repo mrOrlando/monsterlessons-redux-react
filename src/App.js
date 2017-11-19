a4b42f54add06f96f7a7dc74b114af9193c6009a
mrOrlando <juriy88@mail.com>
2017-11-19 15:35:32 +0300

    Show items with react-redux.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
+    console.log('testStore', this.props.testStore);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <input type="text" />
+        <button>Add track</button>
+        <ul>
+          {this.props.testStore.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(state => ({ testStore: state }), dispatch => ({}))(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const initialState = ['Make homework!', 'To smile!'];
+
+function reducer(state = [], action) {
+  console.log('call reducer', state, action);
+  if (action.type === 'ADD_ITEM') {
+    return [...state, action.payload];
+  }
+  return state;
+}
+
+const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
+registerServiceWorker();
+
+/* import { createStore } from 'redux';
 
 function reducer(state = [], action) {
   console.log('call reducer', state, action);
@@ -45,4 +68,4 @@ addTrackButton.addEventListener('click', () => {
   const trackName = trackInput.value;
   console.log('trackName', trackName);
   store.dispatch({ type: 'ADD_ITEM', payload: trackName });
-});
+}); */
