e2084c89ba476ff0cb34fe0ff0cdc19190e6a049
mrOrlando <juriy88@mail.com>
2017-11-19 14:34:30 +0300

    Use redux.

--- src/index.js
+++ src/index.js
@@ -6,3 +6,26 @@
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
+
+import { createStore } from 'redux';
+
+function reducer(state = [], action) {
+  console.log('call reducer', state, action);
+  if (action.type === 'ADD_ITEM') {
+    return [...state, action.payload];
+  }
+  return state;
+}
+
+const store = createStore(
+  reducer,
+  [],
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+store.subscribe(function() {
+  console.log('call subscribe, state:', store.getState());
+});
+
+store.dispatch({ type: 'ADD_ITEM', payload: 'Make homework!' });
+store.dispatch({ type: 'ADD_ITEM', payload: 'To smile!' });
